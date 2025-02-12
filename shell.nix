3f0398e3ed00379593b1b7d7d1207f2121adee94
Daniel Barter <danielbarter@gmail.com>
2025-02-12 11:58:17 -0800

    ..

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 with import <nixpkgs> {}; 
 mkShell {
-  nativeBuildInputs = [ python3 cmake ];
+  nativeBuildInputs = [ python3 cmake python3.pkgs.pybind11 ];
   shellHook = ''
   source env/bin/activate 
   '';
